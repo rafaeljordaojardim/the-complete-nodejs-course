e6cb8269b2bc5e05898fb2dac1eb87a233d3e9a5
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-09-13 09:43:26 -0300

    overview sobre callbacks, arrow functions

---
+++ playground/1-json.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+const argv = require('yargs').argv
+ 
+
+// const book = {
+//     title:'Ego is the Enemy',
+//     author: 'Ryan hokiday'
+// }
+
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+
+
+
+// data.name = "Rafael";
+// data.age = 20;
+
+
+// fs.writeFileSync('1-json.json', JSON.stringify(data));
