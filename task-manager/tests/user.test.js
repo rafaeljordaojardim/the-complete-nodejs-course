9134a031f1d11cfdaa693282ec42c2c0cabb0fd8
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-09-26 09:36:11 -0300

    testing created user and login

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,10 +1,41 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+   await User.deleteMany()
+   await new User(userOne).save()
+})
+
+// afterEach(() => {
+//     console.log('afterEach');
+    
+// })
 
 test('Should signup a new user', async() => {
     await request(app).post('/users').send({
         name:'Rafael j',
         email:'jardin@jordao.com',
-        password:'123456'
+        password:'123456789'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:'sadasdsdasdsa',
+    }).expect(400)
 })
