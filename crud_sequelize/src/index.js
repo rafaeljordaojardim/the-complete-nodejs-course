0bd8b3e055b91d90ef532f3572f36cb485eff73d
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-09-27 14:29:13 -0300

    setting a port with a variable

--- crud_sequelize/src/index.js
+++ crud_sequelize/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const userRoutes = require('./routes/user')
-
+const port = 3001
 app.use(express.json())
 app.use(userRoutes)
 
 
-app.listen(3000, () => {
-    console.log('Running');
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
 })
